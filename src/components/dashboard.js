707a9a87d6df12073f0e26e40cdba0468133e9ba
SafaElmali <tsafaelmali@gmail.com>
2019-09-12 00:30:25 +0300

    added footer

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NavigationBar from './navbar';
 import DashboardDefault from '../pages/DashboardDefault';
 import DashboardAnalytics from '../pages/DashboardAnalytics';
+import Footer from './footer';
 import { Route } from 'react-router-dom';
 import '../assets/styles/main.css';
 
@@ -14,6 +15,9 @@ export default function Dashboard() {
             <div className="content">
                 <Route path="/dashboard/default" component={DashboardDefault} />
                 <Route path="/dashboard/analytics" component={DashboardAnalytics} />
+            </div>
+            <div className="footer-props">
+                <Footer />
             </div>
         </>
     )
---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export default function footer() {
+    return (
+        <>
+            <ul className="footer-item-list">
+                <li>Support</li>
+                <li>Help Center</li>
+                <li>Privacy</li>
+                <li>Privacy</li>
+                <li>Terms of Service</li>
+            </ul>
+            <ul className="mobile-footer-item">
+                <li>© 2019 - Tahsin Safa Elmali</li>
+            </ul>
+        </>
+    )
+}
